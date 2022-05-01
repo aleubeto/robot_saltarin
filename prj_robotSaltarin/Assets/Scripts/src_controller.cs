fb5092a0dba2bb3bb5ade47c13a4c70edb61a592
ale-O-local <alejo.ubeto@gmail.com>
2022-05-01 15:55:20 -0500

    Implementación de puntaje en marcador "score"

--- prj_robotSaltarin/Assets/Scripts/src_controller.cs
+++ prj_robotSaltarin/Assets/Scripts/src_controller.cs
@@ -6,16 +6,22 @@ using UnityEngine.UI;
 public class src_controller : MonoBehaviour
 {
     public Text txt_score;
+    private float topScore = 0.0f;
     private Rigidbody2D rb2d;
     private float moveInput;
     private float speed = 10f;
-    // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-
     }
-    // Update is called once per frame
+    void Update()
+    {
+        if(rb2d.velocity.y > 0 && transform.position.y > topScore)
+        {
+            topScore = transform.position.y;
+        }
+        txt_score.text = "Score: " + Mathf.Round(topScore).ToString();
+    }
     void FixedUpdate()
     {
         moveInput = Input.GetAxis("Horizontal");
