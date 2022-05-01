6db374ef3bd73d1f73c50e95ebabd1334ecf9533
ale-O-local <alejo.ubeto@gmail.com>
2022-05-01 13:38:40 -0500

    Nuevo método de levelGen: mover objetos existentes

--- prj_robotSaltarin/Assets/Scripts/src_proceduralGen.cs
+++ prj_robotSaltarin/Assets/Scripts/src_proceduralGen.cs
@@ -7,6 +7,7 @@ public class src_proceduralGen : MonoBehaviour
     public GameObject player;
     public GameObject platformPrefab;
     public GameObject springPrefab;
+    public GameObject breakablePrefab;
     private GameObject myPlat;
     void Start()
     {
@@ -19,12 +20,67 @@ public class src_proceduralGen : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Método: Mover objetos existentes y crear plataformas especiales
+        int probability = Random.Range(1,10);
+        if(collision.gameObject.name.StartsWith("obj_platform1"))
+        {
+            if (probability == 1)
+            {
+                Destroy(collision.gameObject);
+                Instantiate(springPrefab, new Vector2(Random.Range(-5f, 5f), player.transform.position.y + (12 * Random.Range(0.5f, 0.7f))), Quaternion.identity);
+            }
+            else if (probability > 1 && probability < 4)
+            {
+                Destroy(collision.gameObject);
+                Instantiate(breakablePrefab, new Vector2(Random.Range(-5f, 5f), player.transform.position.y + (12 * Random.Range(0.5f, 0.7f))), Quaternion.identity);
+            }
+            else
+            {
+                collision.gameObject.transform.position = new Vector2(Random.Range(-5f, 5f), player.transform.position.y + (12 * Random.Range(0.5f, 0.7f)));
+            }
+        }
+        else if(collision.gameObject.name.StartsWith("obj_platform2"))
+        {
+            if (probability == 1)
+            {
+                collision.gameObject.transform.position = new Vector2(Random.Range(-5f, 5f), player.transform.position.y + (12 * Random.Range(0.5f, 0.7f)));
+            }
+            else if (probability > 1 && probability < 4)
+            {
+                Destroy(collision.gameObject);
+                Instantiate(breakablePrefab, new Vector2(Random.Range(-5f, 5f), player.transform.position.y + (12 * Random.Range(0.5f, 0.7f))), Quaternion.identity);
+            }
+            else
+            {
+                Destroy(collision.gameObject);
+                Instantiate(platformPrefab, new Vector2(Random.Range(-5f, 5f), player.transform.position.y + (12 * Random.Range(0.5f, 0.7f))), Quaternion.identity);
+            }
+        }
+        else if(collision.gameObject.name.StartsWith("obj_platform3"))
+        {
+            if (probability == 1)
+            {
+                Destroy(collision.gameObject);
+                Instantiate(springPrefab, new Vector2(Random.Range(-5f, 5f), player.transform.position.y + (12 * Random.Range(0.5f, 0.7f))), Quaternion.identity);
+            }
+            else if (probability > 1 && probability < 4)
+            {
+                collision.gameObject.transform.position = new Vector2(Random.Range(-5f, 5f), player.transform.position.y + (12 * Random.Range(0.5f, 0.7f)));
+            }
+            else
+            {
+                Destroy(collision.gameObject);
+                Instantiate(platformPrefab, new Vector2(Random.Range(-5f, 5f), player.transform.position.y + (12 * Random.Range(0.5f, 0.7f))), Quaternion.identity);
+            }
+        }
+
+        /* Método: Destruir y crear objetos
         if(Random.Range(1,10) == 1){
             myPlat = (GameObject)Instantiate(springPrefab, new Vector2(Random.Range(-5f, 5f), player.transform.position.y + (12 * Random.Range(0.5f, 0.7f))), Quaternion.identity);
         }
         else{
             myPlat = (GameObject)Instantiate(platformPrefab, new Vector2(Random.Range(-5f, 5f), player.transform.position.y + (12 * Random.Range(0.5f, 0.7f))), Quaternion.identity);
         }
-        Destroy(collision.gameObject);
+        Destroy(collision.gameObject);*/
     }
 }
